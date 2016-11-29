da00c1f27f8a6aa253efc4fb47d667c6494effe7
Sugandha Gurung <sg00584@surrey.ac.uk>
2016-11-29 21:25:15 +0000

    template 4

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,12 @@ class HomeControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    
+    assert_template layout: 'application'
+    assert_select 'title' , 'My Notes'
+    assert_select 'h1', 'My Notes'
+    assert_select 'p' , 'Welcome to my notes site!'
+    
   end
 
 end
